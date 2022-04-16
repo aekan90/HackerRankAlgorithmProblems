211daf8e7a707058883fe16350c3c2bb14b132df
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-16 11:47:10 +0300

    Problem 2

---
+++ 2_Compare-the-Triplets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2_Compare_the_Triplets
+{
+
+    class Result
+    {
+        private static object list1;
+
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            List<int> list1 = new List<int> { 1, 2, 3 };
+            return list1;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = Result.compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
+
