845f0b5a8e4131e2aa9eb46946485efaf1c9bfa1
Atif Ertugrul Kan <aekan90@users.noreply.github.com>
2022-04-16 14:00:23 +0300

    https://www.hackerrank.com/challenges/compare-the-triplets/problem

--- 2_Compare-the-Triplets/Program.cs
+++ 2_Compare-the-Triplets/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System;
 
 namespace _2_Compare_the_Triplets
 {
@@ -17,22 +17,56 @@ namespace _2_Compare_the_Triplets
          * The function accepts following parameters:
          *  1. INTEGER_ARRAY a
          *  2. INTEGER_ARRAY b
+         *  
+         *  Sample Input
+         *  17 28 30  // alice inputs
+         *  99 16 8   // bob inputs
+         *  Sample Output
+         *  2 1       // alice won
+         *  
          */
 
         public static List<int> compareTriplets(List<int> a, List<int> b)
         {
-            List<int> list1 = new List<int> { 1, 2, 3 };
-            return list1;
+            int AlicePoint = 0;
+            int BobPoint = 0;
+            List<int> result = new List<int>();
+
+            int[] Alice = a.ToArray(); // [4][7][2]
+            int[] Bob = b.ToArray();   // [3][5][9]
+
+            for (int i = 0; i < Alice.Length; i++)
+            {
+                if (Alice[i] > Bob[i])
+                {
+                    AlicePoint++;
+                }
+                if (Alice[i] < Bob[i])
+                {
+                    BobPoint++;
+                }
+                if (Alice[i] == Bob[i])
+                {
+                    // nothing
+                }
+            }
+            result.Add(AlicePoint);
+            result.Add(BobPoint);
+
+            return result;
         }
 
     }
 
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            string fileName = "C:\\Users\\aekan\\source\\repos\\HackerRankAlgorithmProblems\\2_Compare-the-Triplets\\result.txt";
 
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable(fileName));
+
+            StreamWriter textWriter = new StreamWriter(fileName);
 
             List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
 
